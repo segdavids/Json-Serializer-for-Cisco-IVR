709b6aa46aae70a5020f04d688115dac162e795a
David <42349979+segdavids@users.noreply.github.com>
2020-10-12 04:31:35 +0100

    first Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace ConsoleApp1
+
+
+
+namespace ConsoleProgram
 {
-    class Program
+
+
+    public class Class1
     {
-        static void Main(string[] args)
+        public class Account
         {
+            public string Address { get; set; }
+            public string AccountProductCode { get; set; }
+            public string Sex { get; set; }
+            public string CustomerID { get; set; }
+            public string AccountType { get; set; }
+            public string CustomerType { get; set; }
+            public string EmailAddress { get; set; }
+            public string AccountNumber { get; set; }
+            public string AccountStatus { get; set; }
+            public string AccountCurrency { get; set; }
+            public string Phone { get; set; }
+            public string AccountBalance { get; set; }
+            public string FullName { get; set; }
+            public string BVN { get; set; }
+            public string AccountName { get; set; }
+            public string BirthDate { get; set; }
         }
-    }
-}
+        
+
+        static void Main()
+        {
+            HttpClient testcconsole = new HttpClient();
+            testcconsole.BaseAddress = new Uri("http://154.113.16.142:8088/AppDevAPI/");
+            testcconsole.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = testcconsole.GetAsync("api/GetCustomerInfoByAccountNumber/?AccountNumber=1700085845").Result;
+            if (response.IsSuccessStatusCode)
+            {
+
+
+
+                var acctdet = response.Content.ReadAsStringAsync().Result;
+               // var newer = acctdet.TrimEnd(']', '!', '<');
+               // var newer1 = newer.TrimStart('[', '!', '<');
+
+                // Console.WriteLine(acctdet);
+                dynamic jsonRes = JsonConvert.SerializeObject(acctdet);
+                var x = acctdet[10];
+                //itemsArray = JObject.;
+                //Newtonsoft.Json.Linq.JObject Jsonobject = Newtonsoft.Json.Linq.JObject.Parse(acctdet);
+               // jsonRes = jsonRes.Trim("\"".ToCharArray());
+                var test = JsonConvert.DeserializeObject<List<Account>>(jsonRes);
+                Console.WriteLine(test[0].Phone);
+               // Console.WriteLine(acctdet);
+
+
+            }
+
+
+
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                Console.WriteLine("done");
+                Console.ReadLine();
+            }
+            }
+        }
+    } 
+       
